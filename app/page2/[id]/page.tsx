0fb6805aaea0b8aad02a794349e725666d2e905d
Connor Zacharias <zachariasconnor@gmail.com>
2024-09-21 23:41:56 -0700

    further testing 8 prob js assignments that are broken

--- app/page2/[id]/page.tsx
+++ app/page2/[id]/page.tsx
@@ -45,7 +45,7 @@ export default async function Page({params: {id}}: {params : {id : string}}){
             {userData.userInfo.id}
         </div>
         <div>
-            {course.map((course : any) => (
+            {userData.userCourses.map((course : any) => (
                 <div key={course.id}>
                     {course.name}
                 </div>
